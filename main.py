075c56ac04ccb2f9eb8c2dd029dc81bdb71d157e
Krishna Sharma <krishnasharma.dli7@gmail.com>
2025-06-26 19:55:41 +0530

    first commit on repo

---
+++ main.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QApplication
+import sys
+from button_holder import ButtonHolder
+
+
+app = QApplication(sys.argv)
+window = ButtonHolder()
+
+window.show()
+app.exec()
